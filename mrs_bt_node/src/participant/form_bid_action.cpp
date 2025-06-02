5631b9c2f169501f9470c032f17829ce4d890121
MattTidd <mtidd2@unb.ca>
2025-06-02 16:22:47 -0300

    minimal .cpp for form bid action

---
+++ mrs_bt_node/src/participant/form_bid_action.cpp
@@ -0,0 +1,26 @@
+#include <mrs_bt_node/participant/form_bid_action.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace mrs_bt_node
+{
+    // constructor 
+    FormBidAction::FormBidAction(const std::string& name, const BT::NodeConfiguration& config) 
+    : BT::SyncActionNode(name, config)
+    {
+
+    }
+
+    // port listing:
+    BT::PortsList FormBidAction::providedPorts()
+    {
+        return{};
+    }
+
+    // tick method:
+    BT::NodeStatus FormBidAction::tick()
+    {
+        std::cout << 'Formulating Bid...' << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+} // namespace mrs_bt_node
