d051fb4492a282570f161e342040f4e9278abdb7
MattTidd <mtidd2@unb.ca>
2025-06-02 16:23:37 -0300

    minimal .cpp for selected condition

---
+++ mrs_bt_node/src/participant/selected_condition.cpp
@@ -0,0 +1,26 @@
+#include <mrs_bt_node/participant/selected_condition.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace mrs_bt_node
+{
+    // constructor 
+    SelectedCondition::SelectedCondition(const std::string& name, const BT::NodeConfiguration& config) 
+    : BT::ConditionNode(name, config)
+    {
+
+    }
+
+    // port listing:
+    BT::PortsList SelectedCondition::providedPorts()
+    {
+        return{};
+    }
+
+    // tick method:
+    BT::NodeStatus SelectedCondition::tick()
+    {
+        std::cout << 'Selected!' << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+} // namespace mrs_bt_node
