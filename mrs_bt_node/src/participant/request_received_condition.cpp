9f0ca80b3292924805a672177c4ddb81ff33799b
MattTidd <mtidd2@unb.ca>
2025-06-02 16:23:26 -0300

    minimal .cpp for request received condition

---
+++ mrs_bt_node/src/participant/request_received_condition.cpp
@@ -0,0 +1,26 @@
+#include <mrs_bt_node/participant/request_received_condition.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace mrs_bt_node
+{
+    // constructor 
+    RequestReceivedCondition::RequestReceivedCondition(const std::string& name, const BT::NodeConfiguration& config) 
+    : BT::ConditionNode(name, config)
+    {
+
+    }
+
+    // port listing:
+    BT::PortsList RequestReceivedCondition::providedPorts()
+    {
+        return{};
+    }
+
+    // tick method:
+    BT::NodeStatus RequestReceivedCondition::tick()
+    {
+        std::cout << 'Checking if Request Received...' << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+} // namespace mrs_bt_node
