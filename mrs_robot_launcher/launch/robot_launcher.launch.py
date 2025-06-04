6e547602e073049230148f4b857bcada3f92a383
MattTidd <mtidd2@unb.ca>
2025-06-04 13:02:42 -0300

    removed robot name launch argument. set 'use_sim_time' to false temporarily to test JSP. updated x_pose calculation. added opaque function to spawn robots, and added Gazebo

--- mrs_robot_launcher/launch/robot_launcher.launch.py
+++ mrs_robot_launcher/launch/robot_launcher.launch.py
@@ -21,9 +21,9 @@ This launch file brings up a prefixed robot for use in an MRS.
 import os
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
-from launch.conditions import IfCondition
-from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
@@ -33,7 +33,7 @@ def gen_robot_list(number_of_robots):
 
     for i in range(number_of_robots):
         robot_name = "robot" + str(i+1)
-        x_pos = float(i-0.75)
+        x_pos = float((i + 0.25) - 0.75*i)
         robots.append({'name' : robot_name, 'x_pose' : x_pos, 'y_pose': 0.0, 'z_pose' : 0.01})
     
     return robots
@@ -50,41 +50,85 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_sim_time_arg = DeclareLaunchArgument(
         'use_sim_time',
-        default_value = 'true',
-        description = 'Whether to use_sim_time or not, defaults to true.'
+        default_value = 'false',
+        description = 'Whether to use_sim_time or not, defaults to false.'
     )
 
     num_robots = LaunchConfiguration('num_robots')
     num_robots_arg = DeclareLaunchArgument(
         'num_robots',
-        default_value = '2',
+        default_value = '1',
         description = 'Number of robots to spawn within the MRS.'
-    )
-
-    robot_name = LaunchConfiguration('robot_name')
-    robot_name_arg = DeclareLaunchArgument(
-        'robot_name', 
-        default_value = 'robot1',
-        description = 'Name of the robot. Assigned at runtime but needed as a launch argument.'
     )
 
     # paths & params:
     pkg_path = get_package_share_directory('mrs_robot_launcher')
     xacro_path = PathJoinSubstitution([pkg_path, 'urdf', robot_type, 'robot.urdf.xacro'])
-    robot_description = Command(['xacro ' , xacro_path, ' robot_name:=', robot_name]) 
-    rsp_params = {'robot_description' : ParameterValue(robot_description, value_type = str), 'use_sim_time' : use_sim_time}
+    gazebo_params_path = os.path.join(pkg_path, 'config', 'params_gazebo.yaml')
 
-    rsp = Node(
-        package = 'robot_state_publisher',
-        executable = 'robot_state_publisher',
-        output = 'screen',
-        parameters = [rsp_params]
+    # use an opaque function to generate robot launch descriptions:
+    def launch_robots(context):
+        # get list of robots:
+        robots = gen_robot_list(int(num_robots.perform(context)))    # actually get the context of that variable name, turn to int
+        launch_actions = []
+
+        for robot in robots:
+            # generate a URDF with that robot name:
+            robot_description = Command(['xacro ' , xacro_path, ' robot_name:=', robot['name']]) 
+            rsp_params = {'robot_description' : ParameterValue(robot_description, value_type = str), 'use_sim_time' : use_sim_time}
+
+            # robot state publisher:
+            rsp = Node(
+                package = 'robot_state_publisher',
+                executable = 'robot_state_publisher',
+                namespace = robot['name'],
+                parameters = [rsp_params]
+            )
+
+            jsp = Node(
+                package = 'joint_state_publisher',
+                executable = 'joint_state_publisher',
+                name = 'joint_state_publisher',
+                namespace = robot['name'],
+                parameters = [{'use_sim_time' : use_sim_time}]
+
+            )
+
+            print("\n", robot['name'], "has position", 
+                  f"x: {robot['x_pose']} | ", 
+                  f"y: {robot['y_pose']} | ",
+                  f"z: {robot['z_pose']}\n")
+
+            spawner = Node(
+                package = 'gazebo_ros',
+                executable = 'spawn_entity.py',
+                namespace = robot['name'],
+                output = 'screen',
+                arguments = [
+                    '-topic', robot['name'] + 'robot_description',
+                    '-entity', robot_type,
+                    '-x', str(robot['x_pose']),
+                    '-y', str(robot['y_pose']),
+                    '-z', str(robot['z_pose'])
+                ]
+            )
+
+            launch_actions.extend([rsp, jsp, spawner])
+
+        return launch_actions
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments = {
+                # 'verbose' : 'true',
+                'extra_gazebo_args' : '--ros-args --params-file ' + gazebo_params_path}.items()
     )
 
     return LaunchDescription([
         robot_type_arg,
         use_sim_time_arg, 
         num_robots_arg,
-        robot_name_arg,
-        rsp,
+        OpaqueFunction(function = launch_robots), 
+        gazebo
     ])
