02b467b2304b0f2e3e0788f7d27417f4dd2cc2ba
MattTidd <mtidd2@unb.ca>
2025-06-05 13:26:32 -0300

    added spawn protection where robots won't spawn within a set X and Y buffer distance. also added capability assignment where at least one of each type is assigned.

--- mrs_robot_launcher/launch/robot_launcher.launch.py
+++ mrs_robot_launcher/launch/robot_launcher.launch.py
@@ -29,14 +29,75 @@ from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
-def gen_robot_list(number_of_robots):
-
+def gen_robot_list(n_robots, buffer):
     robots = []
+    positions = []
+    buffer_sq = buffer ** 2
+    is_thermal = False
+    is_depth = False
 
-    for i in range(number_of_robots):
+    for i in range(n_robots):
+        placed = False
+        tries = 0
+
+        while not placed and tries < 1000:
+            # generate candidate position:
+            x_pose = round(np.random.uniform(-1.0, 1.0), 3)
+            y_pose = round(np.random.uniform(-1.0, 1.0), 3)
+            too_close = False
+            
+            # check the existing positions:
+            for px, py in positions:
+                dx = x_pose - px
+                dy = y_pose - py
+
+                if dx*dx + dy*dy < buffer_sq:
+                    too_close = True
+                    break
+            
+            # we want it to be not too close:
+            if not too_close:
+                positions.append((x_pose, y_pose))
+                placed = True
+            tries += 1
+        
+        # fallback for never getting placed:
+        if not placed:
+            positions.append((x_pose, y_pose))
+            print(f"Warning: Placed robot at ({x_pose:.2f}, {y_pose:.2f}) without meeting the minimum distance threshold")
+
+        print(f'we are on robot {i+1}/{n_robots}')
+        # capability choice:
+        cap = np.random.choice(['Thermal', 'Depth'])
+        print(f'chosen capability was: {cap}')
+
+        # check value:
+        if cap == 'Thermal':
+            is_thermal = True
+            print(f"setting is_thermal flag to {is_thermal}\n")
+        elif cap == 'Depth':
+            is_depth = True
+            print(f"setting is_depth flag to {is_depth}\n")
+
+        # if there isn't a certain type by the end:
+        if is_depth == False and i+1 == n_robots:
+            cap = 'Depth'
+            print(f'is_depth flag false at end of assignment, set cap to {cap}')
+
+        elif is_thermal == False and i+1 == n_robots:
+            cap = 'Thermal'
+            print(f'is_thermal flag false at end of assignment, set cap to {cap}')
+
+        # make robot list:
         robot_name = "robot" + str(i+1)
-        positions = np.random.uniform(-1.5, 1.5, [1,2])
-        robots.append({'name' : robot_name, 'x_pose' : positions[0,0], 'y_pose': positions[0,1], 'z_pose' : 0.0, 'yaw' : 0.0})
+        robots.append({
+            'name' : robot_name,
+            'x_pose' : x_pose, 
+            'y_pose' : y_pose, 
+            'z_pose' : 0.0,
+            'yaw' : 0.0, 
+            'capability': cap
+        })
     
     return robots
 
@@ -71,12 +132,13 @@ def generate_launch_description():
     # use an opaque function to generate robot launch descriptions:
     def launch_robots(context):
         # get list of robots:
-        robots = gen_robot_list(int(num_robots.perform(context)))    # actually get the context of that variable name, turn to int
+        robots = gen_robot_list(int(num_robots.perform(context)), 0.5)    # actually get the context of that variable name, turn to int
         launch_actions = []
 
         for robot in robots:
             # generate a URDF with that robot name:
             robot_description = Command(['xacro ' , xacro_path, ' robot_name:=', robot['name']]) 
+            print(f"{robot['name']} has capability {robot['capability']}")
             rsp_params = {'robot_description' : ParameterValue(robot_description, value_type = str), 'use_sim_time' : use_sim_time}
 
             # robot state publisher:
