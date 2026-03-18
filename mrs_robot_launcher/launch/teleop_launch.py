b61276153906995a5254a3b92c2b95d82a65a44d
MattTidd <mtidd2@unb.ca>
2026-03-18 10:50:28 -0300

    initial commit for teleop_launch.py, which launches the teleop_twist_keyboard node. this is used to provide cmd_vel topics for testing, but will be replaced by a DRL policy

---
+++ mrs_robot_launcher/launch/teleop_launch.py
@@ -0,0 +1,53 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+
+# what does this file launch?
+# - teleop_twist_keyboard
+
+def generate_launch_description():
+    # define the launch arguments:
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value = "true",
+        description = "Whether or not to use sim time, defaulting to false."
+    )
+
+    stamped = LaunchConfiguration("stamped")
+    stamped_arg = DeclareLaunchArgument(
+        "stamped",
+        default_value = "true",
+        description = "Whether or not to stamp the cmd_vel topic"
+    )
+
+    agent_name = LaunchConfiguration("agent_name")
+    agent_name_arg = DeclareLaunchArgument(
+        "agent_name",
+        default_value = "agent1",
+        description = "The name of the agent to be used, defaulting to agent1"
+    )
+
+    # define the nodes to be launched:
+    teleop = Node(
+        package = "teleop_twist_keyboard",
+        executable = "teleop_twist_keyboard",
+        name = [agent_name, "_teleop_twist_keyboard"],
+        namespace = agent_name,
+        output = "screen",
+        prefix = "xterm -e",
+        parameters = [{"use_sim_time" : use_sim_time, "stamped" : stamped}]
+    )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        stamped_arg,
+        agent_name_arg,
+        teleop
+    ])
