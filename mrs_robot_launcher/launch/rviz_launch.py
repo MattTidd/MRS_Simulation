74d2caf28a2e7fe6f33622a44b23238061fe0976
MattTidd <mtidd2@unb.ca>
2026-03-30 16:59:49 -0300

    update ``rviz_launch.py`` to only launch RVIZ2

--- mrs_robot_launcher/launch/rviz_launch.py
+++ mrs_robot_launcher/launch/rviz_launch.py
@@ -16,7 +16,6 @@ def generate_launch_description():
     # define paths:
     pkg_path = get_package_share_directory("mrs_robot_launcher")
     config_path = os.path.join(pkg_path, "config", "agent_rviz.rviz")
-    xacro_path = PathJoinSubstitution([pkg_path, "urdf", "agent", "agent.urdf.xacro"])
 
     # define the launch arguments:
     use_sim_time = LaunchConfiguration("use_sim_time")
@@ -26,44 +25,11 @@ def generate_launch_description():
         description = "Whether or not to use sim time, defaulting to false"
     )
 
-    agent_type = LaunchConfiguration("agent_type")
-    agent_type_arg = DeclareLaunchArgument(
-        "agent_type",
-        default_value = "typeA",
-        description = "The type of agent to be used"
-    )
-
     agent_name = LaunchConfiguration("agent_name")
     agent_name_arg = DeclareLaunchArgument(
         "agent_name",
         default_value = "agent1",
         description = "The name of the agent to be used"
-    )
-
-    visualize = LaunchConfiguration("visualize")
-    visualize_arg = DeclareLaunchArgument(
-        "visualize",
-        default_value = "true",
-        description = "Whether or not the user is simply visualizing the URDF, which is required for using RVIZ2"
-    )
-
-    # set the required parameters:
-    robot_description = Command(["xacro ", xacro_path, " agent_name:=", agent_name, " agent_type:=", agent_type, " visualize:=", visualize])
-    rsp_parameters = {"robot_description": ParameterValue(robot_description, value_type = str), "use_sim_time" : use_sim_time}
-
-    # nodes:
-    rsp = Node(
-        package = "robot_state_publisher",
-        executable = "robot_state_publisher",
-        namespace = agent_name,
-        parameters = [rsp_parameters],
-    )
-
-    jsp = Node(
-        package = "joint_state_publisher",
-        executable = "joint_state_publisher",
-        namespace = agent_name, 
-        output = "screen", 
     )
 
     rviz = Node(
@@ -77,10 +43,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         use_sim_time_arg,
-        agent_type_arg, 
         agent_name_arg,
-        visualize_arg,
-        rsp, 
-        jsp,
         rviz
     ])
