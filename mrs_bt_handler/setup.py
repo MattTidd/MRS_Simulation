c4b939924036c0978aaad1e16d1690e751f8566b
MattTidd <mtidd2@unb.ca>
2026-04-01 09:38:57 -0300

    initial commit for ``mrs_bt_handler`` package

---
+++ mrs_bt_handler/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'mrs_bt_handler'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='matthew',
+    maintainer_email='mtidd2@unb.ca',
+    description='This package holds the main BT functionality for an agent in the MRS',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
