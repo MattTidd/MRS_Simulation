bfa961e219c9f219a8d4238809c6f961cdfe4e01
MattTidd <mtidd2@unb.ca>
2026-04-03 17:30:59 -0300

    initial commit for ``agent_tree.py``
    - this file hosts the ``create_tree()`` method, which is used to chain several behaviours together into a tree
    - the compiled tree is then returned from this method

---
+++ mrs_bt_handler/mrs_bt_handler/trees/agent_tree.py
@@ -0,0 +1,74 @@
+# import packages:
+import py_trees
+
+# allocation behaviours:
+from mrs_bt_handler.behaviours import (
+    IsSimulationStarted,        # condition for checking if the simulation has started or not
+    ActiveGoal,                 # condition for checking if there is an active goal or not
+    ComputeAndPublishBid,       # action for computing and publishing a bid
+    WaitForAllBids,             # condition for waiting to see if all of the bids have arrived
+    RemainIdle,                 # action for remaining idle when not selected
+    CheckForWin,                # condition for checking to see if a given agent has won the auction
+    NavigateToGoal,             # action for navigating to the goal location
+    RecallAuction               # action for recalling the auction 
+)
+
+# need to define a method for creating trees:
+def create_tree(node, model_path: str = "") -> py_trees.trees.BehaviourTree:
+    # instantiate the condition nodes:
+    is_sim_started      =   IsSimulationStarted(node)
+    active_goal         =   ActiveGoal(node)
+    wait_for_bids       =   WaitForAllBids(node)
+    check_for_win       =   CheckForWin(node)
+
+    # instantiate the action nodes:
+    compute_bid         =   ComputeAndPublishBid(node, model_path)
+    remain_idle         =   RemainIdle(node)
+    navigate_to_goal    =   NavigateToGoal(node)
+    recall_auction      =   RecallAuction(node)
+
+    # main execution tree:
+    execution_tree = py_trees.composites.Selector(
+        name = "ExecutionTree",
+        memory = False, 
+        children = [
+            navigate_to_goal,
+            recall_auction
+        ]
+    )
+
+    # add the tree that checks for agent winning:
+    check_tree = py_trees.composites.Sequence(
+        name = "CheckTree",
+        memory = True,
+        children = [
+            check_for_win, 
+            execution_tree
+        ]
+    )
+
+    # add the main nav tree:
+    nav_tree = py_trees.composites.Selector(
+        name = "MainNav",
+        memory = False,
+        children = [
+            check_tree,
+            remain_idle
+        ]
+    )
+
+    # define the root:
+    root = py_trees.composites.Sequence(
+        name = "Root",
+        memory = True,
+        children = [
+            is_sim_started, 
+            active_goal, 
+            compute_bid, 
+            wait_for_bids, 
+            nav_tree
+        ]
+    )
+
+    # return the completed tree:
+    return py_trees.trees.BehaviourTree(root)
