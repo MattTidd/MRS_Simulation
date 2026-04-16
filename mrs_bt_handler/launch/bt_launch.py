cecac732778c91e8eb8d1d6febc9ea4f1dab69a4
MattTidd <mtidd2@unb.ca>
2026-04-16 10:58:20 -0300

    clean up ``bt_launch.py``
    - remove redundant imports
    - spacing on launch configurations and arguments
    - spaced out node parameters for readability

--- mrs_bt_handler/launch/bt_launch.py
+++ mrs_bt_handler/launch/bt_launch.py
@@ -1,15 +1,13 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, TextSubstitution
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     # define the paths to be used:
-    pkg_path = get_package_share_directory("mrs_bt_handler")
+    pkg_path   = get_package_share_directory("mrs_bt_handler")
     model_path = os.path.join(pkg_path, "suitability_model")
 
     # define the arguments for launching:
@@ -17,58 +15,58 @@ def generate_launch_description():
     agent_name_arg = DeclareLaunchArgument(
         "agent_name",
         default_value = "agent1",
-        description = "Name of the agent to be launched"
+        description   = "Name of the agent to be launched"
     )
 
     agent_type = LaunchConfiguration("agent_type")
     agent_type_arg = DeclareLaunchArgument(
         "agent_type",
         default_value = "typeA",
-        description = "Type of the agent to be launched"
+        description   = "Type of the agent to be launched"
     )
 
     agent_initial_x = LaunchConfiguration("agent_initial_x")
     agent_initial_x_arg = DeclareLaunchArgument(
         "agent_initial_x",
         default_value = "0.0",
-        description = "Initial x position of the agent."
+        description   = "Initial x position of the agent."
     )
 
     agent_initial_y = LaunchConfiguration("agent_initial_y")
     agent_initial_y_arg = DeclareLaunchArgument(
         "agent_initial_y",
         default_value = "0.0",
-        description = "Initial y position of the agent."
+        description   = "Initial y position of the agent."
     )
 
     num_agents = LaunchConfiguration("num_agents")
     num_agents_arg = DeclareLaunchArgument(
         "num_agents",
         default_value = "2",
-        description = "Number of agents in the MRS"
+        description   = "Number of agents in the MRS"
     )
 
     drl_model = LaunchConfiguration("drl_model")
     drl_model_arg = DeclareLaunchArgument(
         "drl_model",
         default_value = "SAC_001",
-        description = "DRL model to be used for the navigational policy"
+        description   = "DRL model to be used for the navigational policy"
     )
 
     # define the node to be launched:
     bt_node = Node(
-        package = "mrs_bt_handler",
-        executable = "bt_node",
-        name = "bt_node",
-        namespace = agent_name,
-        parameters = [{
-            "agent_name"        :    agent_name,
-            "agent_type"        :    agent_type, 
-            "agent_initial_x"   :    agent_initial_x,
-            "agent_initial_y"   :    agent_initial_y,
-            "model_name"        :    drl_model,
-            "num_agents"        :    num_agents,
-            "model_path"        :    model_path
+        package     = "mrs_bt_handler",
+        executable  = "bt_node",
+        name        = "bt_node",
+        namespace   = agent_name,
+        parameters  = [{
+            "agent_name"      : agent_name,
+            "agent_type"      : agent_type, 
+            "agent_initial_x" : agent_initial_x,
+            "agent_initial_y" : agent_initial_y,
+            "model_name"      : drl_model,
+            "num_agents"      : num_agents,
+            "model_path"      : model_path
         }],
     )
 
